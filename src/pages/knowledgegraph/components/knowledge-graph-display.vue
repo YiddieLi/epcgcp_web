f77e144ef1de3b7acb504bc048b28cb48ac7d61e
lipengguang <lpg_1994@163.com>
2019-07-09 00:12:29 +0800

    节点可拖动

--- src/pages/knowledgegraph/components/knowledge-graph-display.vue
+++ src/pages/knowledgegraph/components/knowledge-graph-display.vue
@@ -53,49 +53,60 @@
                     .attr('height', `${height}`);
                 svg.selectAll("*").remove();
 
-                let linkForce = d3
-                    .forceLink()
+                let markerHeight = 12,
+                    markerWidth = 12,
+                    nodeRadius = 40,
+                    refX = 44,
+                    refY = 0,
+                    drSub = nodeRadius + refY;
+
+                let linkForce = d3.forceLink()
                     .id(function (link) {
                         return link.id
-                    })
-                    .strength(0.01);
+                    }).strength(0.01);
 
                 let simulation = d3.forceSimulation()
+                    .nodes(nodes)
                     .force('link', linkForce)
                     .force('charge', d3.forceManyBody().strength(-120))
                     .force('center', d3.forceCenter(width / 2, height / 2));
 
-                let nodeElements = svg.append('g')
-                    .selectAll('circle')
-                    .data(nodes)
-                    .enter()
-                    .append('circle')
-                    .attr('r', 40)
-                    .attr('fill', function (node) {
-                        return node.level === 1 ? '#408080' : '#01a0f1';
-                    });
+                let marker = svg.append("marker")
+                    .attr("id", "resolved")
+                    .attr("markerUnits", "userSpaceOnUse")
+                    .attr("viewBox", "0 -5 10 10")//坐标系的区域
+                    .attr("refX", refX)//箭头坐标
+                    .attr("refY", refY)
+                    .attr("markerWidth", markerWidth)//标识的大小
+                    .attr("markerHeight", markerHeight)
+                    .attr("orient", "auto")//绘制方向，可设定为：auto（自动确认方向）和 角度值
+                    .attr("stroke-width", 1)//箭头宽度
+                    .append("path")
+                    .attr("d", "M0,-5L10,0L0,5")//箭头的路径
+                    .attr('fill', '#000000');//箭头颜色
 
-                let nodeTextElements = svg.append('g')
-                    .selectAll('text')
-                    .data(nodes)
-                    .enter()
-                    .append('text')
-                    .text(function (node) {
-                        return node.label
-                    })
-                    .attr('text-anchor', 'middle')
-                    .attr('font-size', 14)
-                    .attr('x', 0)
-                    .attr('y', 6);
+                let dragDrop = d3.drag().on('start', (node) => {
+                    node.fx = node.x;
+                    node.fy = node.y
+                }).on('drag', (node) => {
+                    simulation.alphaTarget(0.7).restart();
+                    node.fx = d3.event.x;
+                    node.fy = d3.event.y;
+                }).on('end', (node) => {
+                    if (!d3.event.active) {
+                        simulation.alphaTarget(0);
+                    }
+                    node.fx = null;
+                    node.fy = null;
+                });
 
                 let linkElements = svg.append('g')
+                    .attr('class', 'links')
                     .selectAll('line')
                     .data(links)
                     .enter()
                     .append('line')
-                    .attr("marker-end", "url(#resolved)")
-                    .attr('stroke-width', 2)
-                    .attr('stroke', "rgba(50,50,50)");
+                    .attr('marker-end', 'url(#resolved)');
 
                 let linkTextElements = svg.append('g')
                     .selectAll('.relation')
@@ -105,33 +116,47 @@
                     .text(function (link) {
                         return link.relation
                     })
-                    .attr('font-size', 11)
+                    .attr('class', 'line-text')
                     .attr('dx', 0)
                     .attr('dy', 0);
 
-                let marker = svg.append("marker")
-                    .attr("id", "resolved")
-                    .attr("markerUnits", "userSpaceOnUse")
-                    .attr("viewBox", "0 -5 10 10")//坐标系的区域
-                    .attr("refX", 32)//箭头坐标
-                    .attr("refY", -1)
-                    .attr("markerWidth", 12)//标识的大小
-                    .attr("markerHeight", 12)
-                    .attr("orient", "auto")//绘制方向，可设定为：auto（自动确认方向）和 角度值
-                    .attr("stroke-width", 2)//箭头宽度
-                    .append("path")
-                    .attr("d", "M0,-5L10,0L0,5")//箭头的路径
-                    .attr('fill', '#000000');//箭头颜色
+                let nodeElements = svg.append('g')
+                    .selectAll('circle')
+                    .data(nodes)
+                    .enter()
+                    .append('circle')
+                    .attr('class', 'nodes')
+                    .attr('r', nodeRadius)
+                    .attr('fill', function (node) {
+                        return node.level === 1 ? '#408080' : '#01a0f1';
+                    })
+                    .call(dragDrop);
 
-                simulation.nodes(nodes).on('tick', () => {
-                    nodeElements.attr('transform', function (node) {
+                let nodeTextElements = svg.append('g')
+                    .selectAll('text')
+                    .data(nodes)
+                    .enter()
+                    .append('text')
+                    .text(function (node) {
+                        return node.label
+                    })
+                    .attr('class', 'node-text')
+                    .attr('text-anchor', 'middle')
+                    .attr('x', 0)
+                    .attr('y', 6);
+
+                simulation.on('tick', () => {
+                    nodeElements.attr('transform', (node) => {
                         return "translate(" + node.x + "," + node.y + ")";
                     });
-                    nodeTextElements.attr('transform', function (node) {
+                    nodeTextElements.attr('transform', (node) => {
                         return "translate(" + node.x + "," + node.y + ")"
                     });
-                    linkElements.attr('d', function (link) {
-                        return 'M ' + link.source.x + ' ' + link.source.y + ' L ' + link.target.x + ' ' + link.target.y;
+                    linkElements.attr('d', (link) => {
+                        let dx = link.target.x - link.source.x,
+                            dy = (link.target.y - link.source.y),
+                            dr = Math.sqrt(dx * dx + dy * dy);
+                        return "M" + link.source.x + "," + link.source.y + "A" + (dr - drSub) + "," + (dr - drSub) + " 0 0,1 " + link.target.x + "," + link.target.y;
                     }).attr('x1', function (link) {
                         return link.source.x;
                     }).attr('y1', function (link) {
@@ -141,15 +166,8 @@
                     }).attr('y2', function (link) {
                         return link.target.y;
                     });
-                    linkTextElements.attr('transform', function (link) {
-                        if (link.target.x < link.source.x) {
-                            let bBox = this.getBBox();
-                            let rx = bBox.x + bBox.width / 2;
-                            let ry = bBox.y + bBox.height / 2;
-                            return 'rotate(180 ' + rx + ' ' + ry + ')';
-                        } else {
-                            return 'rotate(0)';
-                        }
+                    linkTextElements.attr('transform', (link) => {
+                        return "translate(" + (link.source.x + link.target.x) / 2 + "," + (link.source.y + link.target.y) / 2 + ")"
                     });
                 });
 
@@ -160,6 +178,8 @@
 </script>
 
 <style lang="less">
+    @import "../../../css/global.less";
+
     .knowledge-graph-display {
         height: 100%;
         width: 100%;
@@ -168,6 +188,20 @@
         .svg-area {
             height: 100%;
             width: 100%;
+            .nodes {
+                cursor: default;
+            }
+            .links line {
+                stroke: @darkGreyColor;
+                stroke-opacity: 1;
+                stroke-width: 2;
+            }
+            .node-text {
+                cursor: default;
+            }
+            .line-text {
+                font-size: 11px;
+            }
         }
     }
 </style>
